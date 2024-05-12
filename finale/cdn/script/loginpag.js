28016cbec4cbffcdf9e64a0f1cc216c8935868f0
Luca <luca.soavelucchetta.studente@einaudigramsci.edu.it>
2024-05-12 19:18:13 +0200

    fixato problema opacità-aggiunto bottone chiusura login

--- finale/cdn/script/loginpag.js
+++ finale/cdn/script/loginpag.js
@@ -1,8 +1,13 @@
 let body = document.querySelector('body');
 let profPic = document.querySelector('.profpic');
+let closeL = document.querySelector('.closel');
 
 profPic.addEventListener('click', () => {
-    body.classList.toggle('paginaLogin')
+    body.classList.add('paginaLogin')
     console.log('aperto-popUp-login')
+});
+closeL.addEventListener('click', () => {
+    body.classList.remove('paginaLogin')
+    console.log('chiuso-popUp-login')
 });
 // finisci che quando clicchi opacità di piu e display della pagina di login aggiungendo una classe
